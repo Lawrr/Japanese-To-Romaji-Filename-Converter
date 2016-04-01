fcd96597fe86aa01726b1feef9bc7fb1cc8a0e20
Lawrence Ling <Lawrence1995@hotmail.com>
2016-04-01 12:52:48 +1100

    Added drag and drop and anchoring components

--- Japanese To Romaji File Converter/MainForm.Designer.cs
+++ Japanese To Romaji File Converter/MainForm.Designer.cs
@@ -23,13 +23,39 @@
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
+            this.FilesBox = new System.Windows.Forms.ListBox();
+            this.ConvertBTN = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // FilesBox
+            // 
+            this.FilesBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.FilesBox.FormattingEnabled = true;
+            this.FilesBox.Location = new System.Drawing.Point(13, 13);
+            this.FilesBox.Name = "FilesBox";
+            this.FilesBox.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            this.FilesBox.Size = new System.Drawing.Size(504, 446);
+            this.FilesBox.TabIndex = 0;
+            // 
+            // ConvertBTN
+            // 
+            this.ConvertBTN.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.ConvertBTN.Location = new System.Drawing.Point(523, 437);
+            this.ConvertBTN.Name = "ConvertBTN";
+            this.ConvertBTN.Size = new System.Drawing.Size(75, 23);
+            this.ConvertBTN.TabIndex = 1;
+            this.ConvertBTN.Text = "Convert";
+            this.ConvertBTN.UseVisualStyleBackColor = true;
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(610, 469);
+            this.Controls.Add(this.ConvertBTN);
+            this.Controls.Add(this.FilesBox);
             this.Name = "MainForm";
             this.Text = "Japanese to Romaji File Converter";
             this.Load += new System.EventHandler(this.MainForm_Load);
@@ -38,6 +64,9 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox FilesBox;
+        private System.Windows.Forms.Button ConvertBTN;
     }
 }
 
--- Japanese To Romaji File Converter/MainForm.cs
+++ Japanese To Romaji File Converter/MainForm.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,30 @@ namespace Japanese_To_Romaji_File_Converter {
     public partial class MainForm : Form {
         public MainForm() {
             InitializeComponent();
+
+            FilesBox.AllowDrop = true;
+            FilesBox.DragEnter += new DragEventHandler(FilesBox_DragEnter);
+            FilesBox.DragDrop += new DragEventHandler(FilesBox_DragDrop);
+        }
+
+        private void FilesBox_DragEnter(object sender, DragEventArgs e) {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop)) {
+                e.Effect = DragDropEffects.Copy;
+            }
+        }
+
+        private void FilesBox_DragDrop(object sender, DragEventArgs e) {
+            string[] items = (string[])e.Data.GetData(DataFormats.FileDrop);
+            foreach (string item in items) {
+                if (Directory.Exists(item)) {
+                    string[] directoryFiles = Directory.GetFiles(item, "*", SearchOption.AllDirectories);
+                    foreach (string file in directoryFiles) {
+                        FilesBox.Items.Add(file);
+                    }
+                } else {
+                    FilesBox.Items.Add(item);
+                }
+            }
         }
 
         private void MainForm_Load(object sender, EventArgs e) {
@@ -19,5 +44,6 @@ namespace Japanese_To_Romaji_File_Converter {
             BringToFront();
             Activate();
         }
+
     }
 }
