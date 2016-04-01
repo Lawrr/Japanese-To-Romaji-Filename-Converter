c50235e0f9b98dc04696b5e229384b2fc4e9d4e7
Lawrence Ling <5894666+Lawrr@users.noreply.github.com>
2016-04-02 00:33:58 +1100

    Moved IsAscii to extensions, fixed decoding html encoding

--- Japanese To Romaji File Converter/Extensions.cs
+++ Japanese To Romaji File Converter/Extensions.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 using System.Windows.Forms;
 
 namespace Japanese_To_Romaji_File_Converter {
@@ -17,6 +18,11 @@ namespace Japanese_To_Romaji_File_Converter {
                     action();
                 }
             }
+        }
+
+        public static bool IsAscii(this string value) {
+            // ASCII encoding replaces non-ascii with question marks, so we use UTF8 to see if multi-byte sequences are there
+            return Encoding.UTF8.GetByteCount(value) == value.Length;
         }
 
     }
