79e8fb1efd21a34cc02104a3f135530cd6885ff1
Lawrence Ling <5894666+Lawrr@users.noreply.github.com>
2016-04-02 16:46:51 +1100

    Now checks if it is already romanized before translating

--- Japanese To Romaji File Converter/Converter.cs
+++ Japanese To Romaji File Converter/Converter.cs
@@ -13,6 +13,21 @@ namespace JapaneseToRomajiFileConverter {
         public static string LanguagePair = "ja|en";
         public static char MapChar = '`';
 
+        // TODO store in better data structure?
+        // Unicode ranges for each set
+        public static int RomajiMin = 0x0020;
+        public static int RomajiMax = 0x007E;
+        public static int HiraganaMin = 0x3040;
+        public static int HiraganaMax = 0x309F;
+        public static int KatakanaMin = 0x30A0;
+        public static int KatakanaMax = 0x30FF;
+        public static int KanjiMin = 0x4E00;
+        public static int KanjiMax = 0x9FBF;
+        // Covers Basic Latin, Latin-1 Supplement, Extended A, Extended B
+        public static int LatinMin = 0x0000;
+        public static int LatinMax = 0x024F;
+
+
         public event EventHandler<ProgressEventArgs> Progress;
 
         private List<string> Files;
@@ -67,16 +82,16 @@ namespace JapaneseToRomajiFileConverter {
             OnProgressEvent(ProgressEvent.Completed);
         }
 
-        private string Translate(string inText) {
-            // Check null
-            if (inText == null) {
-                return "";
-            }
+        private static bool IsRomanized(string text) {
+            return text.Where(c => c >= LatinMin && c <= LatinMax).Count() == text.Length;
+        }
 
-            // Check if already translated
-            if (inText.IsAscii()) {
-                return inText;
-            }
+        private static string Translate(string inText) {
+            // Check null
+            if (inText == null) return "";
+
+            // Check if already romanized
+            if (IsRomanized(inText)) return inText;
 
             // Map english characters to substitutes
             Tuple<string, List<char>> charMap = MapChars(inText);
@@ -102,7 +117,7 @@ namespace JapaneseToRomajiFileConverter {
             return outText;
         }
 
-        private Tuple<string, List<char>> MapChars(string text) {
+        private static Tuple<string, List<char>> MapChars(string text) {
             // Replace characters with sub chars for the translation
             List<char> mapChars = new List<char>();
             StringBuilder mapText = new StringBuilder(text);
@@ -110,7 +125,7 @@ namespace JapaneseToRomajiFileConverter {
             // Loop through each character and map english with $MapChar
             for (int i = 0; i < text.Length; i++) {
                 char currChar = text[i];
-                if (currChar.ToString().IsAscii()) {
+                if (IsRomanized(currChar.ToString())) {
                     mapChars.Add(currChar);
                     mapText[i] = MapChar;
                 } else if (currChar == MapChar) {
@@ -125,7 +140,7 @@ namespace JapaneseToRomajiFileConverter {
             return Tuple.Create(mapText.ToString(), mapChars);
         }
 
-        private string UnmapChars(string text, List<char> mapChars) {
+        private static string UnmapChars(string text, List<char> mapChars) {
             StringBuilder unmapText = new StringBuilder(text);
 
             // Loop through each character and unmap mapped chars
--- Japanese To Romaji File Converter/Extensions.cs
+++ Japanese To Romaji File Converter/Extensions.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Text;
 using System.Windows.Forms;
 
 namespace JapaneseToRomajiFileConverter {
@@ -18,11 +17,6 @@ namespace JapaneseToRomajiFileConverter {
                     action();
                 }
             }
-        }
-
-        public static bool IsAscii(this string value) {
-            // ASCII encoding replaces non-ascii with question marks, so we use UTF8 to see if multi-byte sequences are there
-            return Encoding.UTF8.GetByteCount(value) == value.Length;
         }
 
     }
