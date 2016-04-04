6f2cca8439f8774483d02227cf8b464eb37d2699
Lawrence Ling <Lawrence1995@hotmail.com>
2016-04-04 22:42:43 +1000

    Changed some static variables to const

--- JapaneseToRomajiFilenameConverter/Converter/Maps.cs
+++ JapaneseToRomajiFilenameConverter/Converter/Maps.cs
@@ -1,9 +1,9 @@
 ﻿namespace JapaneseToRomajiFileConverter.Converter {
     public static class Maps {
 
-        public static string DirectoryPath = "res/maps/";
+        public const string DirectoryPath = "res/maps/";
 
-        public static string JaLatn = "ja-Latn_maps.txt";
+        public const string JaLatn = "ja-Latn_maps.txt";
 
     }
 }
